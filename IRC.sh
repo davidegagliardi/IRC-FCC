9ceef429abc22d9bf594c874c2319604c52e8cd6
Davide Gagliardi <davide@webgraphical.it>
2020-05-12 16:15:05 +0200

    Update IRC.sh

--- IRC.sh
+++ IRC.sh
@@ -1,3 +1,21 @@
-DEMO_USERNAME=IRC-user
-DEMO_PASSWORD=IRC
-DEMO_PROJECTN=IRC-project
+P_USERNAME=IRC-user
+P_PASSWORD=IRC
+P_PROJECTN=IRC-project
+
+# Project creation and user creation
+openstack project create --description "This is IRC Project" "${P_PROJECTN}"
+openstack user create --password "${P_PASSWORD}" "${P_USERNAME}"
+openstack role add --project "${P_PROJECTN}" --user "${P_USERNAME}" "member"
+
+#Get Ubuntu imahe and create the image
+wget -P /var/tmp -c http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/alpine-standard-3.11.6-x86_64.iso
+openstack image create --disk-format qcow2 --container-format bare --public --file /var/tmp/alpine-standard-3.11.6-x86_64.iso alpine-standard-3.11.6-x86_64
+
+#Create flavor
+openstack flavor create --ram 512 --disk 5 --ephemeral 5 --vcpus 1 --public micro.alpine
+openstack flavor create --ram 1024 --disk 5 --ephemeral 5 --vcpus 1 --public mini.alpine
+
+#Network
+openstack network create net1
+openstack subnet create --network net1 --subnet-range 10.11.12.0/24 --dns-nameserver 208.67.222.222 --dns-nameserver 208.67.220.220 subnet1
+openstack router create router1
