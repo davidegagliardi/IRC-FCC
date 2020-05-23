a089048e09670a8e5dd750c6300475557a9e14fc
aabriani <44431611+aabriani@users.noreply.github.com>
2020-05-23 12:50:29 +0200

    added word count bash-regex-scripting.sh

--- Automation/bash-regex-scripting.sh
+++ Automation/bash-regex-scripting.sh
@@ -1,8 +1,8 @@
-# SINGLE server regex network IP extraction (example: 10.11.12.143)
+# SINGLE server regex network IP extraction (example output: 10.11.12.143)
 openstack server show UbuntuServer -c addresses --format value | grep -Po '10\.11\.12\.[[:digit:]]*'
 
 # MULTIPLE server regex network IP extraction
-# (example:
+# (example output:
 # 10.11.12.188
 # 10.11.12.143
 # 10.11.12.63
@@ -15,3 +15,7 @@ openstack server list -c Networks -c Name --format value | grep -Po '10\.11\.12\
 # STORE MULTIPLE server regex network IP extraction in ONE VARIABLE (IPs stored with space as interspace)
 # (example: 10.11.12.188 10.11.12.143 10.11.12.63 10.11.12.122 10.11.12.240 10.11.12.4)
 VAR=$(openstack server list -c Networks -c Name --format value | grep -Po '10\.11\.12\.[[:digit:]]*')
+
+# COUNT number of words stored in a variable (example output: 6)
+wc -w <<< $VAR
+# alternative in case <<< is not supported: echo "$VAR" | wc -w
