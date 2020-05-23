3552b51c8f5502aec0306f126f57427c96419bd1
aabriani <44431611+aabriani@users.noreply.github.com>
2020-05-23 17:55:47 +0200

    update create-new-ircd-instance.sh

--- Automation/create-new-ircd-instance.sh
+++ Automation/create-new-ircd-instance.sh
@@ -1,9 +1,14 @@
 source login-as-admin.sh
 EX_IRCD=$(openstack server list -c Networks -c Name --format value | grep -Po '10\.11\.12\.[[:digit:]]*')
-wc -w <<< $EX_IRCD
-openstack server create --flavor mini.ubuntu --image IRC-Server-Image --network netIRC --security-group default IRC-Server-$EX_IRCD
+OLD_ID=$(wc -w <<< $EX_IRCD)
+NEW_ID=$((OLD_ID + 1))
+openstack server create --flavor mini.ubuntu --image IRC-Server-Image --network netIRC --security-group default IRC-Server-$NEW_ID
 $FLOATIP_NEWIRCD=
-ssh -t ubuntu@$FLOATIP_NEWIRCD 'sed;sed'
 
-ssh -t ubuntu@$FLOATIP_NEWIRCD 'sed -i 's/<define name="servername" value="change">/yesssss/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf'
-sed -i 's/<define name="servername" value="change">/yesssss/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf
+# working command from within the instance
+#sed -i 's/<define name="servername" value="change">/yesssss/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf
+
+# working command from the VM to the instance
+#ssh -i cloud.key ubuntu@172.24.4.194 -t "sed -i 's/<define name=\"servername\" value=\"change\">/<define name=\"servername\" value=\"ircserver$NEW_ID\">/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf"
+
+ssh -i cloud.key ubuntu@$FLOATIP_NEWIRCD -t "sed -i 's/<define name=\"servername\" value=\"change\">/<define name=\"servername\" value=\"ircserver$NEW_ID\">/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf"
