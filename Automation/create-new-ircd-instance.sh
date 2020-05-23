95b07d0be52a450129ee838a0bc18f4e80667182
aabriani <44431611+aabriani@users.noreply.github.com>
2020-05-23 15:04:03 +0200

    added sed pseudocode

--- Automation/create-new-ircd-instance.sh
+++ Automation/create-new-ircd-instance.sh
@@ -1,3 +1,9 @@
 source login-as-admin.sh
 EX_IRCD=$(openstack server list -c Networks -c Name --format value | grep -Po '10\.11\.12\.[[:digit:]]*')
-openstack server create --flavor mini.ubuntu --image IRC-Server-Image --network netIRC --security-group default IRC-Server-1
+wc -w <<< $EX_IRCD
+openstack server create --flavor mini.ubuntu --image IRC-Server-Image --network netIRC --security-group default IRC-Server-$EX_IRCD
+$FLOATIP_NEWIRCD=
+ssh -t ubuntu@$FLOATIP_NEWIRCD 'sed;sed'
+
+ssh -t ubuntu@$FLOATIP_NEWIRCD 'sed -i 's/<define name="servername" value="change">/yesssss/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf'
+sed -i 's/<define name="servername" value="change">/yesssss/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf
