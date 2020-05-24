3b409cef4c10c138208c4938b304b06626bc22d2
aabriani <44431611+aabriani@users.noreply.github.com>
2020-05-24 18:29:06 +0200

    automated hubs config of new leaf link

--- Automation/create-new-ircd-instance.sh
+++ Automation/create-new-ircd-instance.sh
@@ -15,4 +15,15 @@ openstack server add floating ip --fixed-ip-address $IP_VM IRC-Server-$NEW_ID $F
 # working command from the VM to the instance
 #ssh -i cloud.key ubuntu@172.24.4.194 -t "sed -i 's/<define name=\"servername\" value=\"change\">/<define name=\"servername\" value=\"ircserver$NEW_ID\">/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf"
 
+# insert new leaf link to hubs from within the instance
+#sed 'N a <LINE-TO-BE-ADDED>' FILE.txt
+
+#sed -i "1i <link name=\"ircserver$NEW_ID.omega.example.org\" ipaddr=\"$IP_VM\" port=\"7000\" allowmask=\"10.11.12.0/25\" timeout=\"2m\" statshidden=\"no\" hidden=\"no\" sendpass=\"password2\" recvpass=\"password1\">" /home/ubuntu/inspircd-3.6.0/run/conf/links.conf
+#ssh -i cloud.key ubuntu@172.24.4.194 -t "sed -i '1i <link name=\"ircserver$NEW_ID.omega.example.org\" ipaddr=\"$IP_VM\" port=\"7000\" allowmask=\"10.11.12.0/25\" timeout=\"2m\" statshidden=\"no\" hidden=\"no\" sendpass=\"password2\" recvpass=\"password1\">' /home/ubuntu/inspircd-3.6.0/run/conf/links.conf"
+
+# give new IRCD leaf link config to both hubs
+ssh -i cloud.key ubuntu@$FLOATIP_HUB1 -t "sed -i '1i <link name=\"ircserver$NEW_ID.omega.example.org\" ipaddr=\"$IP_VM\" port=\"7000\" allowmask=\"10.11.12.0/25\" timeout=\"2m\" statshidden=\"no\" hidden=\"no\" sendpass=\"password2\" recvpass=\"password1\">' /home/ubuntu/inspircd-3.6.0/run/conf/links.conf"
+ssh -i cloud.key ubuntu@$FLOATIP_HUB2 -t "sed -i '1i <link name=\"ircserver$NEW_ID.omega.example.org\" ipaddr=\"$IP_VM\" port=\"7000\" allowmask=\"10.11.12.0/25\" timeout=\"2m\" statshidden=\"no\" hidden=\"no\" sendpass=\"password2\" recvpass=\"password1\">' /home/ubuntu/inspircd-3.6.0/run/conf/links.conf"
+
+# give appropriate name to IRCD leaf just configured 
 ssh -i cloud.key ubuntu@$FLOATIP_NEWIRCD -t "sed -i 's/<define name=\"servername\" value=\"change\">/<define name=\"servername\" value=\"ircserver$NEW_ID\">/g' /home/ubuntu/inspircd-3.6.0/run/conf/inspircd.conf"
