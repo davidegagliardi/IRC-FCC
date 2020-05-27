34a112257ec393227bf9d490996e102226bd7e91
aabriani <44431611+aabriani@users.noreply.github.com>
2020-05-27 13:44:32 +0200

    update k8s irc-client deployment

--- Weechat.sh
+++ Weechat.sh
@@ -11,5 +11,5 @@ weechat -t irc://172.24.4.200/#hola
 weechat -t irc://172.24.4.200
 weechat -t irc://172.24.4.200/#hola
 weechat -t irc://davide@172.24.4.200/#hola
-weechat --run-command "/set irc.server_default.nicks "user1,user2,user3,user4,user5,user6,user7,user,8,user9,user10,user11,user12,user13,user14,user15,user16,user17,user18,user19,user20""
+weechat --run-command "/set irc.server_default.nicks "user1,user2,user3,user4,user5,user6,user7,user,8,user9,user10,user11,user12,user13,user14,user15,user16,user17,user18,user19,user20";/set irc.server_default.addresses "172.24.4.200,172.24.4.201,172.24.4.150,172.24.4.151,172.24.4.152,172.24.4.153";/set irc.server_default.autojoin "172.24.4.200""
 weechat irc://172.24.4.200/#hola
